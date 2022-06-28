bcef8a2c2e643c7004afe1fa0c9799635be0fb2e
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-06-29 00:20:53 +0600

    button as well as icon added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import  SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
   return (
     <div className="App">
-     <Button variant="contained">Hello World</Button>
+      <header className='App-header'>
+        <ButtonGroup size="small" aria-label="small button group">
+          <Button 
+              onClick={() => {
+              alert('Save it.');
+              }}
+              startIcon={<SaveIcon />}
+
+              variant="contained"
+              size="medium"
+              color="secondary"
+              disableElevation
+          >Save</Button>
+
+          <Button 
+              onClick={() => {
+              alert('Delete it.');
+              }}
+              startIcon={<DeleteIcon />}
+
+              variant="contained"
+              size="medium"
+              color="secondary"
+              disableElevation
+          >Delete</Button>
+        </ButtonGroup>
+      </header>
     </div>
   );
 }
